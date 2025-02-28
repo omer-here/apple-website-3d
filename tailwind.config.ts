86f8894b9d2702f244f4d690d7c6d88af16dbf4b
omer-here <110015265+omer-here@users.noreply.github.com>
2025-02-28 17:44:37 +0500

    First Working Instance but have bugs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "#101010",
       },
     },
   },
